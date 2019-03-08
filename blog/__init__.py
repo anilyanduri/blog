9b144814a7e062813387a0431b4ef139bc68e634
Anil Yanduri <anilkumaryln@gmail.com>
2019-03-08 12:16:29 +0530

    added custom 500

--- blog/__init__.py
+++ blog/__init__.py
@@ -12,7 +12,11 @@ db = SQLAlchemy()
 
 
 def page_not_found(e):
-  return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
+
+def something_went_wrong(e):
+    return render_template('500.html'), 500
 
 
 def create_app(config_name):
@@ -20,6 +24,7 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, something_went_wrong)
     from blog import models
     db.init_app(app)
     migrate = Migrate(app, db)
